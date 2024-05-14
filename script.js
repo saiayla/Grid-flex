104d2a8292fec30e73fdeb97f6de682e7f9f4538
saiayla <151695408+saiayla@users.noreply.github.com>
2024-05-14 20:32:26 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,24 +1,26 @@
-console.log('Olá! Eu estou funcionando.');
+function addTask(e) {
+    e.preventDefault();
+    let tarefa = document.getElementById('task');
 
-function calcular () {
-  let n1= document.getElementById('n1').value;
-  let n2 = document.getElementById('n2').value;
-  let op = document.getElementById('operation').value;
-  let soma = parseInt(n1) + parseInt(n2);
-  let subtrair = parseInt(n1) - parseInt(n2);
-  let multiplicar = parseInt(n1) * parseInt(n2);
-  let dividir = parseInt(n1) / parseInt(n2);
+    if(tarefa.value.trim() != '') {
+        let novo_li = document.createElement('li');
+        novo_li.innerText = tarefa.value;
 
-  if(op == "somar") {
-    alert('A soma foi ' + soma);
-  }
-  else if(op == "subtrair") {
-    alert('A subtração foi ' + subtrair);
-  }
-  else if(op == "multiplicar") {
-    alert('A multiplicação foi ' + multiplicar);
-  }
-  else if(op == "dividir") {
-    alert('A divisão foi ' + dividir);
-  }
+        let lista = document.getElementById('tasklist');
+        lista.appendChild(novo_li);
+        tarefa.value='';
+
+        let message = document.getElementById('message');
+        message.innerText = 'Sucesso: Tarefa incluída!' ;
+        message.className = 'sucess-message';
+    } else {
+        let message = document.getElementById('message');
+        message.innerText = 'Erro: Preencha a descrição da tarefa!' ;
+        message.className = 'error-message'; 
+   }
 };
+
+function clearMessage() {
+    let message = document.getElementById('message');
+    message.innerHTML = '&nbsp;';
+}
