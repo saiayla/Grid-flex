c801755a319797470046e2669bc032e473fd9c7f
saiayla <151695408+saiayla@users.noreply.github.com>
2024-05-23 17:33:53 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -2,7 +2,6 @@ let taskList = [];
 
 function addTask(event) {
     event.preventDefault();
-    let button = document.getElementById('button');
     let description = document.getElementById('description');
     if (description.value == '') {
         showMessage();
@@ -37,9 +36,16 @@ function updateTasks() {
         let newOl = document.createElement('ol');
         button.disabled = false;
 
-        taskList.forEach((task) => {
+        taskList.forEach((task, index) => {
             let newLi = document.createElement('li');
             newLi.innerText = task;
+            let removeButton = document.createElement('button');
+            removeButton.className = 'remove-button';
+            removeButton.innerText = 'Remover';
+            removeButton.addEventListener('click', () => {
+                removeTask(index);
+            });
+            newLi.appendChild(removeButton);
             newOl.appendChild(newLi);
         });
         divTasks.replaceChildren(newOl);
@@ -55,3 +61,43 @@ function removeAll() {
     taskList = [];
     updateTasks();
 }
+
+function highlightTask() {
+    if (taskList.length > 0) {
+        let randomIndex = Math.floor((Math.random() * taskList.length) + 0);
+        let randomTask = document.querySelectorAll('#tasks ol li');
+        randomTask[randomIndex].style.color = 'white';
+        randomTask[randomIndex].style.fontWeight = 'bold';
+        randomTask[randomIndex].style.fontSize = '24px';
+    }
+}
+
+function removeTask(index) {
+    taskList.splice(index, 1);
+    updateTasks();
+}
+
+function filterTask() {
+    let searchInput = document.getElementById('search');
+    let filterText = searchInput.value.toLowerCase();
+    let tasks = document.querySelectorAll('#tasks ol li');
+  
+    let hasResult = false;
+  
+    tasks.forEach((task) => {
+      let taskText = task.innerText.toLowerCase();
+      if (taskText.includes(filterText)) {
+        task.style.display = 'block';
+        hasResult = true;
+      }
+      else {
+        task.style.display = 'none'; 
+      }
+    });
+  
+    if (!hasResult) {
+      let noResultText = document.createElement('p');
+      noResultText.innerText = 'Não existe resultado para essa pesquisa.';
+      document.getElementById('tasks').appendChild(noResultText);
+    }
+  }
